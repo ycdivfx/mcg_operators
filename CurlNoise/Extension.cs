2651c929c2b6b1b873e41f672de7ba43a941b05d
Daniel Santana <danielgsantana@gmail.com>
2018-07-03 11:28:42 +0100

    Added CurlNoise aka SimplexNoise

---
+++ CurlNoise/Extension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ViperEngine;
+
+namespace CurlNoiseExtension
+{
+    public class Extension : IViperExtension
+    {
+        public void RegisterOps()
+        {
+            ViperOp.RegisterStaticMethods(typeof(Random.CurlNoise));
+        }
+    }
+}
